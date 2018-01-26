52a7449b39540f9138538ac6513ab666b3281e1e
David Hale <davidhale@Davids-MacBook-Air.local>
2018-01-26 20:03:18 +0000

    Adding individual inputs for tracker

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -2,6 +2,8 @@ import React from 'react';
 import InputSlider from 'react-input-slider';
 import { Link } from 'react-router-dom';
 
+import fire from '../firebase';
+
 class Tracker extends React.Component {
   constructor(props) {
     super(props);
@@ -9,32 +11,45 @@ class Tracker extends React.Component {
     this.state = {
       moodValue: 50
     }
+
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+    fire.database().ref('moods').push( this.state.moodValue );
+    this.state.moodValue = 50;
   }
 
   render() {
     return (
-      <div className="tracker-wrapper">
-        <div className="input-section">
-          <div className="input-wrapper">
-            <h3>Mood</h3>
-            <input type="range" value={this.state.value} onChange={event => { this.setState({ moodValue: event.target.value }) }} />
-          </div>
-          <div className="input-wrapper">
-            <h3>Anxiety</h3>
-            <input type="range" value={this.state.value} onChange={event => { this.setState({ moodValue: event.target.value }) }} />
-          </div>
-          <div className="input-wrapper">
-            <h3>Exercise</h3>
-            <input type="range" value={this.state.value} onChange={event => { this.setState({ moodValue: event.target.value }) }} />
-          </div>
-          <div className="input-wrapper">
-            <h3>Appetite</h3>
-            <input type="range" value={this.state.value} onChange={event => { this.setState({ moodValue: event.target.value }) }} />
-          </div>
-        </div>
-        <div className="graph-wrapper">
-        </div>
-      </div>
+      <Link to="/tracker/input">How was your day?</Link>
+      // <div className="tracker-wrapper">
+      //   <div className="input-section">
+      //     <div className="input-wrapper">
+      //       <h3>Mood</h3>
+      //       <form onSubmit={this.onSubmit}>
+      //         <input type="range" value={this.state.value} onChange={event => { this.setState({ moodValue: event.target.value }) }} />
+      //         <button type="submit">Save</button>
+      //       </form>
+      //     </div>
+      //     <div className="input-wrapper">
+      //       <h3>Anxiety</h3>
+      //       <input type="range" value={this.state.value} onChange={event => { this.setState({ moodValue: event.target.value }) }} />
+      //     </div>
+      //     <div className="input-wrapper">
+      //       <h3>Exercise</h3>
+      //       <input type="range" value={this.state.value} onChange={event => { this.setState({ moodValue: event.target.value }) }} />
+      //     </div>
+      //     <div className="input-wrapper">
+      //       <h3>Appetite</h3>
+      //       <input type="range" value={this.state.value} onChange={event => { this.setState({ moodValue: event.target.value }) }} />
+      //     </div>
+      //   </div>
+      //   <div className="graph-wrapper">
+      //   </div>
+      // </div>
     );
   }
 }
---
+++ src/components/TrackerInput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TrackerInput extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello dave
+      </div>
+    );
+  }
+}
+
+export default TrackerInput;
