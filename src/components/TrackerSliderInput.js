032a050b2ddad5835a954c2ed407b675f1eba1e6
David Hale <davidhale@Davids-MacBook-Air.local>
2018-01-27 14:06:23 +0000

    styling slider icons

--- src/components/TrackerSliderInput.js
+++ src/components/TrackerSliderInput.js
@@ -5,13 +5,18 @@ class TrackerSliderInput extends React.Component {
     return (
       <div className="input-card card">
         <h2>{ this.props.title }</h2>
-        <input 
-          type="range" 
-          value={this.props.value} 
-          onChange={ this.props.onChange } 
-          min="0"
-          max="10"
-        />
+        <div className="slider-row-wrapper">
+          <img src="/resources/sad.png" className="slider-icon" alt=""/>
+          <input 
+            className=""
+            type="range" 
+            value={this.props.value} 
+            onChange={ this.props.onChange } 
+            min="0"
+            max="10"
+          />
+          <img src="/resources/happy.png" className="slider-icon" alt=""/>
+        </div>
         <button className="btn btn-primary" onClick={ this.props.onClick }>Add</button>
       </div>
     );
