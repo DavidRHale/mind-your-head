7a767e3671ec3c322b577e50ce973c00c9b97e6e
David Hale <davidhale@Davids-MacBook-Air.local>
2018-01-28 12:06:20 +0000

    random stuff

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -67,23 +67,23 @@ class Tracker extends React.Component {
           <div className="advice-wrapper">
             <div className="advice-panel dark-blue-border">
               <h4>Mood: { this.state.scoresToday.mood }</h4>
-              <p>Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
+              <p>Looks like you're feeling low today. Try some gratitude exercises. <strong>Write down 3 things you are grateful for.</strong></p>
             </div>
             <div className="advice-panel light-blue-border">
               <h4>Anxiety: { this.state.scoresToday.anxiety }</h4>            
-              <p>Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
+              <p>Seems like your anxiety is troubling you just now. Try using <a href="https://www.noisli.com/">Noisli</a> in the background to help calm yourself down.</p>
             </div>
             <div className="advice-panel green-border">
               <h4>Sleep: { this.state.scoresToday.sleep }</h4>            
-              <p>Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
+              <p>Sleep wasn't great for you last night. If you have trouble drifting off, try some deep breathing exercises before bed.</p>
             </div>
             <div className="advice-panel rose-border">
               <h4>Social: { this.state.scoresToday.social }</h4>              
-              <p>Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
+              <p>Your social interaction doesn't seem like too much of a problem today. If this is something you want to build on, meet up with a friend or family member you haven't seen for a while.</p>
             </div>
             <div className="advice-panel yellow-border">
               <h4>Exercise: { this.state.scoresToday.exercise }</h4>            
-              <p>Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
+              <p>You haven't exercised for a while which can make your mood drop. It doesn't need to be a lot, so start simple with a short 30 minute walk.</p>
             </div>
           </div>      
         </div>
@@ -91,8 +91,8 @@ class Tracker extends React.Component {
     } else {
       return (
         <div className="card advice-input-wrapper">
-          <div className="advice-panel">
-            Enter your tracking stuff and we can give you some advice!
+          <div className="advice-panel-empty">
+            <p className="input-prompt">Tell us about your symptoms and we can give you some advice!</p>
           </div>
           <Link to="/tracker/input" className="btn btn-primary input-start-button">How was your day?</Link>  
         </div>      
